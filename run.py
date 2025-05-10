15df082d8ca504c660f4cc68f1b58fb9c7826c3d
jtraveler <matthew.jtraveler@gmail.com>
2025-05-10 06:07:51 +0200

    Added CIF and import duty logic. However a bug is causing an error

--- run.py
+++ run.py
@@ -18,6 +18,55 @@ SHEET = GSPREAD_CLIENT.open('code-institute-project3-import_rates')
 
 country_rates = SHEET.worksheet('imported_rates')
 
-data_test = country_rates.get_all_values()
+data = country_rates.get_all_values()
 
-print(data_test)
+print(data)
+
+# Convert sheet data to list dictionaries
+headers = data [0]
+rows = data[1:]
+
+structured_data = []
+for row in rows:
+    country_dict = {}
+    for set in range(len(headers)):
+        key = headers[set]
+        value = row[set]
+        #Try converting numbers if possible
+        try:
+            value = float(value) if value not in ['TRUE', 'FALSE'] else value
+        except ValueError:
+            pass
+        country_dict[key] = value
+    structured_data.append(country_dict)
+
+#Prompt user input
+print("Available countries:")
+for entry in structured_data:
+    print(".", entry['country_code'])
+
+selected_country = input("Please enter destination country code (e.g. US): ").upper()
+
+#Find matching country
+country_info = None
+for entry in structured_data:
+    if entry['country_code'] == selected_country:
+        country_info = entry
+        break
+if not country_info:
+    print("Sorry, country not found in the sheet.")
+    exit()
+
+#Ask for product cost inputs
+product_value = float(input("Enter product value (in USD): "))
+shipping_cost = float(input("Enter shipping cost (in USD): "))
+insurance_cost = float(input("Enter insurance cost (in USD): "))
+
+#Calculating CIF (Cost, Insurance, and Freight)
+cif_total = product_value + shipping_cost + insurance_cost
+
+import_duty = cif_total * country_info['duty']
+
+print(f"\n CIF Total: ${cif_total:.2f}")
+print(f"\n Import Duty: ${cif_duty:.2f}")
+          
