7d79a61fdefc5c3c33dcbbdf5f34f80f620c09cf
jtraveler <matthew.jtraveler@gmail.com>
2025-05-11 06:45:50 +0200

    Refactored the files so that the Google Sheets connection is now in a new file called imports_rates.py

--- run.py
+++ run.py
@@ -2,42 +2,12 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-import gspread
-from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# Import and load structured country rate data from Google Sheets
+from import_rates import load_import_data
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('code-institute-project3-import_rates')
+structured_data = load_import_data()
 
-country_rates = SHEET.worksheet('imported_rates')
-
-data = country_rates.get_all_values()
-
-
-# Convert sheet data to list dictionaries
-headers = data [0]
-rows = data[1:]
-
-structured_data = []
-for row in rows:
-    country_dict = {}
-    for set in range(len(headers)):
-        key = headers[set]
-        value = row[set]
-        #Try converting numbers if possible
-        try:
-            value = float(value) if value not in ['TRUE', 'FALSE'] else value
-        except ValueError:
-            pass
-        country_dict[key] = value
-    structured_data.append(country_dict)
 
 #Prompt user input
 print("Available countries:")
